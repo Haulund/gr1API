63674186a96c59d405ade914c6e622990a5a5269
Haulund <steffen.haulund@gmail.com>
2020-04-22 10:11:38 +0200

    first test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel - STEFFEN
                 </div>
 
                 <div class="links">
